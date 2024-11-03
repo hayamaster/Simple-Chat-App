ab3bc654d266b2e088623dd64a9541d0a00ea4db
hayamaster <dnfxmfkdbzos@naver.com>
2024-11-03 15:45:38 -0500

    feat: create checkPassword api

---
+++ server/controller/checkPassword.js
@@ -0,0 +1,48 @@
+const UserModel = require("../models/UserModel");
+const bcryptjs = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+async function checkPassword(req, res) {
+  try {
+    const { password, userId } = req.body;
+
+    const user = await UserModel.findById(userId);
+
+    // check password
+    const verifyPassword = await bcryptjs.compare(password, user.password);
+
+    if (!verifyPassword) {
+      return res.status(400).json({
+        message: "Please check password",
+        error: true,
+      });
+    }
+
+    // create token using jwt
+    const tokenData = {
+      id: user._id,
+      email: user.email,
+    };
+    const token = jwt.sign(tokenData, process.env.JWT_SECRET_KEY, {
+      expiresIn: "1d",
+    });
+
+    const cookieOption = {
+      http: true,
+      secure: true,
+    };
+
+    return res.cookie("token", token, cookieOption).status(200).json({
+      message: "Login successfully",
+      token,
+      success: true,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message || error,
+      error: true,
+    });
+  }
+}
+
+module.exports = checkPassword;
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const registerUser = require("../controller/registerUser");
 const checkEmail = require("../controller/checkEmail");
+const checkPassword = require("../controller/checkPassword");
 
 const router = express.Router();
 
@@ -8,5 +9,7 @@ const router = express.Router();
 router.post("/register", registerUser);
 // check user email
 router.post("/email", checkEmail);
+// check user password
+router.post("/password", checkPassword);
 
 module.exports = router;
