7e015211d5358d6dd51a7ef1eb6139794f55038f
hayamaster <dnfxmfkdbzos@naver.com>
2024-11-03 15:17:54 -0500

    feat: create checkEmail api

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const registerUser = require("../controller/registerUser");
+const checkEmail = require("../controller/checkEmail");
 
 const router = express.Router();
 
 // create user api
 router.post("/register", registerUser);
+// check user email
+router.post("/email", checkEmail);
 
 module.exports = router;
