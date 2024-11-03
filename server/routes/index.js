63494088768e726c22a281e1a5e1bc0f0944c858
hayamaster <dnfxmfkdbzos@naver.com>
2024-11-03 16:42:04 -0500

    feat: create updateUserDetail api

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,6 +4,7 @@ const checkEmail = require("../controller/checkEmail");
 const checkPassword = require("../controller/checkPassword");
 const userDetails = require("../controller/userDetails");
 const logout = require("../controller/logout");
+const updateUserDetails = require("../controller/updateUserDetails");
 
 const router = express.Router();
 
@@ -17,5 +18,7 @@ router.post("/password", checkPassword);
 router.get("/user-details", userDetails);
 // logout user
 router.get("/logout", logout);
+// update user
+router.post("/update-user", updateUserDetails);
 
 module.exports = router;
