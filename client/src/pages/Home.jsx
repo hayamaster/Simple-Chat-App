da39b4b301d33311ccf899611833ac3a72aaea65
hayamaster <dnfxmfkdbzos@naver.com>
2024-11-06 16:33:40 -0500

    feat: add userDetail api in Home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,23 @@
 import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import axios from "axios";
 
 const Home = () => {
+  const fetchUserDetails = async () => {
+    try {
+      const URL = `${process.env.REACT_APP_BACKEND_URL}/api/user-details`;
+      const response = await axios.get(URL, { withCredentials: true });
+
+      console.log(response.data);
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchUserDetails();
+  }, []);
+
   return (
     <div className="text-2xl">
       <h1>Home</h1>
