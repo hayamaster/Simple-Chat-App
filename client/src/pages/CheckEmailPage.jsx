e1efa5a9f4f48f8c1961ec9465d14359b87d77f9
hayamaster <dnfxmfkdbzos@naver.com>
2024-11-06 03:46:05 -0500

    feat: create CheckEmailPage

--- client/src/pages/CheckEmailPage.jsx
+++ client/src/pages/CheckEmailPage.jsx
@@ -1,7 +1,84 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { PiUserCircle } from "react-icons/pi";
+
 const CheckEmailPage = () => {
+  const [data, setData] = useState({
+    email: "",
+  });
+  const navigate = useNavigate();
+
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+
+    setData((prev) => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const URL = `${process.env.REACT_APP_BACKEND_URL}/api/email`;
+
+    try {
+      const res = await axios.post(URL, data);
+      toast.success(res.data.message);
+
+      if (res.data.success) {
+        setData({
+          email: "",
+        });
+        navigate("/password");
+      }
+    } catch (error) {
+      toast.error(error?.response?.data?.message);
+    }
+  };
+
   return (
-    <div>
-      <h1>Check your email</h1>
+    <div className="mt-5">
+      <div className="bg-white w-full max-w-md rounded overflow-hidden p-4 mx-auto">
+        <div className="w-fit mx-auto mb-2">
+          <PiUserCircle size={80} />
+        </div>
+        <h3>Welcome to Chat App!</h3>
+
+        <form className="grid gap-4 mt-3" onSubmit={handleSubmit}>
+          <div className="flex flex-col gap-1">
+            <label htmlFor="email">Email :</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="enter your email"
+              className="bg-slate-100 px-2 py-1 focus:outline-primary"
+              value={data.email}
+              onChange={handleOnChange}
+              required
+            />
+          </div>
+
+          <button className="bg-secondary text-lg px-4 py-1 hover:bg-[#37898c] rounded mt-2 font-bold text-white leading-relaxed tracking-wide">
+            Let's Go
+          </button>
+        </form>
+
+        <p className="my-3 text-center">
+          New User ?{" "}
+          <Link
+            to={"/register"}
+            className="hover:text-secondary hover:underline font-semibold"
+          >
+            Register
+          </Link>{" "}
+        </p>
+      </div>
     </div>
   );
 };
