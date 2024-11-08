24bffda5ee262432ad1a89f017973749f9694a67
hayamaster <dnfxmfkdbzos@naver.com>
2024-11-08 05:12:00 -0500

    feat: modify sidebar css for responsive & add logo in Home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,14 +1,17 @@
-import { Outlet, useNavigate } from "react-router-dom";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import { useEffect, useCallback } from "react";
 import axios from "axios";
 import { useSelector, useDispatch } from "react-redux";
 import { logout, setUser } from "../redux/userSlice";
 import Sidebar from "../components/Sidebar";
+import logo from "../assets/chat_app_logo.png";
 
 const Home = () => {
   const user = useSelector((state) => state.user);
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const location = useLocation();
+  const basePath = location.pathname === "/";
 
   console.log("redux user", user);
 
@@ -19,7 +22,7 @@ const Home = () => {
 
       dispatch(setUser(response.data.data));
 
-      if (response.data.logout) {
+      if (response.data.data.logout) {
         dispatch(logout());
         navigate("/email");
       }
@@ -34,13 +37,22 @@ const Home = () => {
 
   return (
     <div className="grid lg:grid-cols-[300px,1fr] h-screen max-h-screen">
-      <section className="bg-white">
+      <section className={`bg-white ${!basePath && "hidden"} lg:block`}>
         <Sidebar />
       </section>
 
-      <section>
+      <section className={`${basePath && "hidden"}`}>
         <Outlet />
       </section>
+
+      <div className="lg:flex flex-col gap-2 justify-center items-center hidden">
+        <div>
+          <img src={logo} width={250} alt="logo" />
+        </div>
+        <p className="text-lg mt-2 text-slate-500">
+          Select user to send message
+        </p>
+      </div>
     </div>
   );
 };
